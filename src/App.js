9cfad40faa3bda3ffaf93a8e1063b079fba90e22
Gaston Irby III <gaston@gastons-imac.lan>
2021-11-12 11:12:20 -0800

    Set up structure. nearly passing all tests.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+// import Home from "./Home";
+import Form from "./pizza";
+
+
 
 const App = () => {
+
+ 
+
   return (
-    <>
+    <header className="homepage">
+      <img src="./Assets/Pizza.jpg" alt="pizza img" />
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+        <BrowserRouter>
+          <nav>
+            <Link to="/"><button id="home-button">Home</button></Link>
+            {/* <Route path="/" /> */}
+            <Link to="/pizza"><button id="order-pizza">Order Pizza!</button></Link>
+          </nav>
+          <Switch>
+            <Route path="/pizza" component={Form} />
+          </Switch>
+        </BrowserRouter>
+    </header>
   );
 };
 export default App;
